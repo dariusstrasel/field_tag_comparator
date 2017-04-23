bb8402e5f1c7b20825171fa0e3f4e2aa6b6119cd
Darius Strasel <strasel.darius@gmail.com>
2017-04-23 16:29:05 -0400

    Refactor 'old' and 'new' references to 'left' and 'right'; refactor Engine IO reference to 'file_mapping'; refactor omitted/added field calculators.

--- models.py
+++ models.py
@@ -5,9 +5,9 @@ from datetime import datetime
 
 class IO:
     """Abstracts logic surrounding the use and manipulation of input data."""
-    def __init__(self, old_file_path, new_file_path, output_file_path):
-        self.OLD_FILE_PATH = path.abspath(old_file_path)
-        self.NEW_FILE_PATH = path.abspath(new_file_path)
+    def __init__(self, left_file_path, right_file_path, output_file_path):
+        self.LEFT_FILE_PATH = path.abspath(left_file_path)
+        self.RIGHT_FILE_PATH = path.abspath(right_file_path)
         self.OUTPUT_FILE_PATH = path.abspath(output_file_path)
         self.total_omitted = 0
         self.total_added = 0
@@ -19,16 +19,16 @@ class IO:
     def get_shared_tags(self):
         """Returns a sorted list of Tag objects that pass is_shared() inspection."""
         result = []
-        with open(self.OLD_FILE_PATH, 'r') as old_file:
-            for old_line in old_file:
-                with open(self.NEW_FILE_PATH, 'r') as new_file:
-                    for new_line in new_file:
-                        current_tag = Tag(old_line, new_line)
-                        if current_tag.is_shared_tag():
-                            self.total_fields += len(current_tag.existing_field_value)
+        with open(self.LEFT_FILE_PATH, 'r') as left_file:
+            for left_line in left_file:
+                with open(self.RIGHT_FILE_PATH, 'r') as right_file:
+                    for right_line in right_file:
+                        combined_tag = Tag(left_line, right_line)
+                        if combined_tag.is_shared_tag():
+                            self.total_fields += len(combined_tag.left_fields)
                             self.field_changes += (
-                                len(current_tag.added_field_values) - len(current_tag.omitted_field_values))
-                            result.append(current_tag)
+                                len(combined_tag.added_field_values) - len(combined_tag.omitted_field_values))
+                            result.append(combined_tag)
         sorted_result = sorted(result, key=lambda k: k.left_tag_name)
         return sorted_result
 
@@ -38,15 +38,15 @@ class IO:
         if not os.path.isdir(self.OUTPUT_FILE_PATH):
             os.mkdir(self.OUTPUT_FILE_PATH)
         with open(path.join(self.OUTPUT_FILE_PATH, 'output.txt'), 'w') as output_file:
-            header_schema = """Date: {}, New_File: {}, Old_File: {}, Total Shared Tags: {}, Tag Differences: {}\n"""
-            header_output = header_schema.format(
+            header_schema = """Date: {}, Left_File: {}, Right_File: {}, Total Shared Tags: {}, Tag Differences: {}\n"""
+            header_rendered = header_schema.format(
                 str(datetime.now()),
-                self.NEW_FILE_PATH,
-                self.OLD_FILE_PATH,
+                self.RIGHT_FILE_PATH,
+                self.LEFT_FILE_PATH,
                 len(self.shared_tags),
                 "% " + str((((self.total_fields + self.field_changes) - self.total_fields) / self.total_fields) * 100)
             )
-            output_file.write(header_output)
+            output_file.write(header_rendered)
             for tag in self.shared_tags:
                 print(tag)
                 output_file.write(str(tag))
@@ -57,8 +57,8 @@ class Tag:
     def __init__(self, left_tag_line, right_tag_line):
         self.left_tag_name = self.extract_tag(left_tag_line)
         self.right_tag_name = self.extract_tag(right_tag_line)
-        self.existing_field_value = self.extract_field(left_tag_line)
-        self.new_field_value = self.extract_field(right_tag_line)
+        self.left_fields = self.extract_field(left_tag_line)
+        self.right_fields = self.extract_field(right_tag_line)
         if self.is_shared_tag():
             self.omitted_field_values = self.get_field_omissions()
             self.added_field_values = self.get_field_additions()
@@ -67,26 +67,26 @@ class Tag:
         """Used to format sane schema for stdout."""
         output_schema = """
 Tag Name: {}
-Existing Values: {}
-New Values: {}
+Left File Fields: {}
+Right File Fields: {}
 Fields Omitted: {}
 Fields Added: {}
 """
         output_tag_name = self.left_tag_name
-        output_existing_field_value = " ".join(self.existing_field_value)
-        output_new_field_value = " ".join(self.new_field_value)
+        output_left_fields = " ".join(self.left_fields)
+        output_right_fields = " ".join(self.right_fields)
         output_omitted_field_values = " ".join(self.omitted_field_values)
         output_added_field_values = " ".join(self.added_field_values)
 
-        output = output_schema.format(output_tag_name, output_existing_field_value, output_new_field_value,
+        output_rendered = output_schema.format(output_tag_name, output_left_fields, output_right_fields,
                                       output_omitted_field_values, output_added_field_values)
 
-        return str(output)
+        return str(output_rendered)
 
     def is_shared_tag(self) -> bool:
-        """Evaluates a bool to determine if two input tags have the same name."""
+        """Evaluates to a bool to determine if two input tags have the same name."""
         if self.left_tag_name == self.right_tag_name:
-            if self.existing_field_value != self.new_field_value:
+            if self.left_fields != self.right_fields:
                 return True
         else:
             return False
@@ -112,29 +112,29 @@ Fields Added: {}
     def get_field_omissions(self) -> list:
         """Evaluates a tag objects field omissions by comparing existing and new
         field values."""
-        result = []
-        for field in self.existing_field_value:
-            if field not in self.new_field_value:
-                result.append(field)
-        return result
+        field_omissions = []
+        for field in self.left_fields:
+            if field not in self.right_fields:
+                field_omissions.append(field)
+        return field_omissions
 
     def get_field_additions(self) -> list:
         """Evaluates a tag objects field additions by comparing existing and new
                 field values."""
-        result = []
-        for field in self.new_field_value:
-            if field not in self.existing_field_value:
-                result.append(field)
-        return result
+        field_additions = []
+        for field in self.right_fields:
+            if field not in self.left_fields:
+                field_additions.append(field)
+        return field_additions
 
 
 class Engine:
     """Simple abstraction used to define easy source of program manipulation."""
-    def __init__(self, old_file_path, new_file_path, output_file_path):
-        self.input_output = IO(old_file_path, new_file_path, output_file_path)
+    def __init__(self, left_file_path, right_file_path, output_file_path):
+        self.file_mapping = IO(left_file_path, right_file_path, output_file_path)
 
     def start(self):
-        self.input_output.get_shared_tags()
+        self.file_mapping.get_shared_tags()
 
     def save_output(self):
-        self.input_output.save_tags_to_output()
+        self.file_mapping.save_tags_to_output()
